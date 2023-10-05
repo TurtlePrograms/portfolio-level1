1427c555c4a7708e46bd42356eb34b79ac81be64
David <111139186+TurtlePrograms@users.noreply.github.com>
2023-10-05 13:58:44 +0200

    update on code

--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-function checkWindowSize() {
-  //gets the navbar element
-  const navbarElement = document.querySelector('.navbar');
 
-  // Check if the 'navbar' element exists
-  if (navbarElement) {
-    // Get the width of the 'navbar' element
-    const navbarWidth = navbarElement.getBoundingClientRect().width;
-
-    // Get the current window width
-    const windowWidth = window.innerWidth;
-
-    // Compare window width with 'navbar' width
-    if (windowWidth < navbarWidth) {
-      // Hide the 'navbar' element when the window is smaller
-      navbarElement.style.display = 'none';
-    } else {
-      // Show the 'navbar' element when the window is larger
-      navbarElement.style.display = 'block';
-    }
-  } else {
-    // If no element with class 'navbar' is found, log an error message
-    console.log("Element with class 'navbar' not found.");
-  }
-}
-
-// Call the function to check the window size
-checkWindowSize();
-
-// Add an event listener that waits for the window to resize
-window.addEventListener('resize', checkWindowSize)
 
 
 //list
