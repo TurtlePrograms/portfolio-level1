a0ecee2901e27b90204c26ac12b1a546ca0e200c
David <111139186+TurtlePrograms@users.noreply.github.com>
2023-10-23 15:19:20 +0200

    added extra text display for imags

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -17,6 +17,8 @@ function showSlides(n) {
   let dots = document.getElementsByClassName("dots");
   let captionText = document.getElementById("caption");
   let numberText = document.getElementsByClassName("numberText");
+  let imageText = document.getElementsByClassName("storie");
+  let storydisplay = document.getElementsByClassName("storydisplay");
 
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
@@ -30,4 +32,6 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
   numberText.innerHTML = slideIndex + "/" + slides.length;
+
+  storydisplay[0].innerHTML = imageText[slideIndex-1].innerHTML;
 } 
