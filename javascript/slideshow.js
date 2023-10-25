ccafbf2f2a03c37e74190c9cc8826a4c1abf7129
David <111139186+TurtlePrograms@users.noreply.github.com>
2023-10-25 11:08:00 +0200

    slideshow is not auto when no interact

--- javascript/slideshow.js
+++ javascript/slideshow.js
@@ -11,6 +11,24 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+// Automatic Slideshow - change image every 5 seconds
+let slideInterval = setInterval(function() {
+  plusSlides(1);
+}, 2000); 
+
+// Pause the slideshow when the user interacts with it
+let slideshowContainer = document.getElementsByClassName("container")[0];
+slideshowContainer.addEventListener("mouseenter", function() {
+  clearInterval(slideInterval);
+});
+
+// Resume the slideshow when the user stops interacting with it
+slideshowContainer.addEventListener("mouseleave", function() {
+  slideInterval = setInterval(function() {
+    plusSlides(1);
+  }, 2000); 
+});
+
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("images");
@@ -31,5 +49,5 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
   numberText.innerHTML = slideIndex + "/" + slides.length;
+}
 
-} 
